c1031f1cb143941867ca8ae02ad35bd9a8df2408
VagTsop <vatsop52@gmail.com>
2024-12-02 12:14:58 +0200

    rollback to Angular 17 @for and @if syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from './tasks/tasks.component';
-import { NgFor, NgIf } from '@angular/common';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -20,7 +19,7 @@ export class AppComponent {
     this.selectedUserId = id;
   }
 
-  get selectedUser() {
+  get selectedUser()  {
     return this.users.find((user) => user.id === this.selectedUserId)!;
   }
 }
