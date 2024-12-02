a3f8d214c985397e585166e199c41068869d69c2
VagTsop <vatsop52@gmail.com>
2024-12-02 12:00:45 +0200

    Outputting Conditional Content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from "./tasks/tasks.component";
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
@@ -13,13 +13,13 @@ import { TasksComponent } from "./tasks/tasks.component";
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   onSelectUser(id: string) {
     this.selectedUserId = id;
   }
 
-  get selectedUser()  {
+  get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
   }
 }
