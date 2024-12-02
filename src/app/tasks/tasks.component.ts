87e33c3a652ce71d99132c2e5ba56264708ff2d9
VagTsop <vatsop52@gmail.com>
2024-12-02 10:26:08 +0200

    TypeScript: Working With Potentially Undefined Values & Union Types

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -8,5 +8,16 @@ import { Component, Input } from '@angular/core';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input({ required: true }) name!: string;
-}
+  //? string or undefined
+  @Input({ required: true }) name?: string;
+
+   //! --> name is required and you are assertingg that it
+   // will be not null or undefined. ypu do not need to handle
+   // undefined checks in you code because you are telling typescript
+   // that name will always be defined.
+   // ! -> does not enforce tha the value it actually provided at runtime
+   // it is your responsibility to ensure that name is initiliazed before it is used
+   // if name is not initilized, it will lead to runtime errors
+   //@Input({ required: true }) name!: string;
+  }
+
