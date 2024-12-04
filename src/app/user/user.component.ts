9e1630039f04de6a6b8fef09149a9d756fc080d2
VagTsop <vatsop52@gmail.com>
2024-12-04 11:49:22 +0200

    Dynamic CSS Styling with Class Bindings

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -16,6 +16,7 @@ import { User } from './user.model';
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
 
   @Output() select = new EventEmitter<string>();
 
