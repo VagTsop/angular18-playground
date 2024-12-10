b2d7c4c82ad98215428c60506afd8e17d471bc6a
VagTsop <vatsop52@gmail.com>
2024-12-10 11:58:22 +0200

    Using the Submitted Data

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
@@ -10,15 +11,20 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
-  enteredTitle = signal('');
-  enteredDate = signal('');
-  enteredSummary = signal('');
+  @Output() add = new EventEmitter<NewTaskData>();
+  enteredTitle = '';
+  enteredDate = '';
+  enteredSummary = '';
 
   onCancel() {
     this.cancel.emit();
   }
 
   onSubmit() {
-    
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    });
   }
 }
